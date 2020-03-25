0bd480d52344dc240c77586544ed4c6c0bab237d
Debayan Sutradhar <debayansutradhar3@gmail.com>
2020-03-25 21:22:30 +0530

    updated stuff

--- src/main/java/StreamPiClient/Main.java
+++ src/main/java/StreamPiClient/Main.java
@@ -1,10 +1,7 @@
 package StreamPiClient;
 
 import javafx.application.Application;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
